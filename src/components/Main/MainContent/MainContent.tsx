380534478726ae50d96aee7d2437f006a8d6be75
GoncharenkoVitaliy <Vitalesss.87@gmail.com>
2024-02-19 20:14:00 +0200

    first commit

---
+++ src/components/Main/MainContent/MainContent.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import styles from './MainContent.module.css';
+import axios from 'axios';
+import Spinner from '../../spinner/Spinner';
+import CardsProduct from '../../CardsProduct/CardsProduct';
+
+export interface ISetDate {
+   id: number,
+   image: string,
+   categories: string,
+   prace: number,
+   popular: boolean,
+   title: string,
+   description: {
+      discOn?: string,
+      discTwo?: string,
+      discThree?: string,
+      discFour?: string,
+      discFive?: string,
+      discSix?: string,
+      discSeven?: string,
+      discEight?: string
+   }
+}
+
+const MainContent = () => {
+   const [data, setData] = useState<ISetDate[]>([]); 
+   const [loading, setLoading] = useState(true);
+
+   const getUsers = async() => {
+      try{
+         const res = await axios.get("http://localhost:3031/product");
+         setData(res.data)
+         setLoading(false);
+      }
+      catch(err) {
+         // Error handling
+         setLoading(false);
+         console.log(err);
+         return null;
+      }
+   };
+
+   useEffect(() => {
+         getUsers();
+   }, [loading]);
+
+   return (
+      <section className={styles.main__content}>
+         <h2 className="hidden">Товары</h2>
+         {loading
+            ? <Spinner/>
+            :  <ul className={styles.content__product}>
+                  {data.map((d) => {
+                     return (
+                        <CardsProduct d={d}  key={d.id}/>
+                     )
+                  })}
+               </ul>
+         }
+      </section>
+   )
+}
+
+export default MainContent
